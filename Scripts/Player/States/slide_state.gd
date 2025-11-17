872e91015f1d4c938c3a01e49c7537f540a81622
AleksaFreelanceIt <118134232+AleksaFreelanceIt@users.noreply.github.com>
2025-11-17 23:49:18 +0100

    Fixing parameters for pixel art size movement

--- Scripts/Player/States/slide_state.gd
+++ Scripts/Player/States/slide_state.gd
@@ -3,45 +3,37 @@ class_name SlideState
 
 @export var slide_initial_speed: float = 600
 @export var slide_stop: float = 200
-
+@export var slide_deceleration: float
 func enter():
 	print("Entering slide state")
 	var character = state_machine.get_parent()
-	print(character.velocity.x)
-	if character.velocity.x > 0:
-		character.velocity.x = slide_initial_speed
-	else:
-		character.velocity.x = -slide_initial_speed
-
-func exit():
-	pass
-
-func update(delta: float):
-	pass
+	
+	# Set initial slide velocity
+	var current_direction = sign(character.velocity.x)
+	if current_direction == 0:
+		current_direction = 1 if character.facing_right else -1
+	
+	character.velocity.x = slide_initial_speed * current_direction
 
 func physics_update(delta: float):
 	var character = state_machine.get_parent()
 	var direction = Input.get_axis("Left","Right")
-	print(character.velocity.x)
-	if character.velocity.x > 0:
-		if character.velocity.x > slide_stop:
-			character.velocity.x -= 10
-		else:
-			state_machine.change_state("Crouch")
-	else:
-		if character.velocity.x < -slide_stop:
-			character.velocity.x += 10
-		else:
-			state_machine.change_state("Crouch")
 	
-	if character.velocity.y > 0:
+	# Usporenje
+	character.velocity.x = move_toward(character.velocity.x, slide_stop, slide_deceleration * delta)
+	
+	# Check for state transitions only when slide is complete
+	if abs(character.velocity.x) <= slide_stop:  # Small threshold
+		if character.is_on_floor():
+			if direction != 0:
+				state_machine.change_state("Sprint")
+			else:
+				state_machine.change_state("Crouch")
+		else:
+			state_machine.change_state("Fall")
+	
+	# Handle falling during slide
+	if !character.is_on_floor() and character.velocity.y > 0:
 		state_machine.change_state("Fall")
 	
 	character.move_and_slide()
-	if character.is_on_floor():
-		if direction != 0:
-			state_machine.change_state("Walk")
-		else:
-			state_machine.change_state("Idle")
-func handle_input(event: InputEvent):
-	pass
